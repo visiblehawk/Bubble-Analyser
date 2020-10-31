b4baff7000363b55dd9bce0f8d838c1e6459acf9
Francisco Reyes <frreyes@uc.cl>
2020-10-31 17:53:29 +1000

    + Created a file (main.m) to mimic the workflow, later we need to change this so it can use the GUI
    +Changed BV_quantification.m into a subrutine, we probably need to move some other pieces of code into subrutines later
    
    + Added some sample images we can share with the code so people can use them
    
    Signed-off-by: Francisco Reyes <frreyes@uc.cl>

---
+++ gui/main.m
@@ -0,0 +1,113 @@
+% function main(path_to_folder)
+%main.m - Bubble sizer image processing code, soon to be the GUI
+%This script calls all other functions/subrutines.
+%At some point we need to change this into a GUI asociated M-file
+%
+% Syntax:  main(path_to_folder)
+%
+% Inputs:
+%    path_to_folder - path to the folder with the images to be analysed
+%    
+% Outputs:
+%    none
+%
+% Example: 
+%    main('./Sample_photos/')
+%
+% Author: Reyes, Francisco; Quintanilla, Paulina; Mesa, Diego
+% email: f.reyes@uq.edu.au,  
+% Website: https://gitlab.com/frreyes1/bubble-sizer
+% Oct-2020;
+%------------- BEGIN CODE --------------
+
+%% Load files in folder
+%check if path has a final slash ##TODO. check if it works on linux
+if(path_to_folder(end)~='/')
+    path_to_folder = [path_to_folder '/']; %add it if not
+end
+%Load file names in folder
+folder_files = dir(path_to_folder); 
+folder_files = folder_files(~[folder_files.isdir]); %exclude folders
+
+%% Separate image files from whatever else it might be there in the folder
+%figure out the extension if the image files (.jpeg, tiff, png, etc...)
+[~, ~, fextensions] = cellfun(@fileparts, {folder_files.name}, 'UniformOutput', false);
+[s, ~, j]=unique(fextensions);
+img_extension = s{mode(j)};
+%with the extension we can now get the image file list
+img_files = dir([path_to_folder '*' img_extension]);
+clear s j folder_files fextensions %remove unused variables
+
+%% Load sample image and calibration images
+%The code uses two calibration images, one is for doing background
+%correction if lighting is uneven throughout the field of view. The second
+%one is for calibrating the resolution, i.e how many pixels per mm. We also
+%show a random image to show the processing results before running the
+%batch quantification, similar to a sandbox test.
+
+%create figures to show claibration and sample images
+%calibration, a subplot to show both
+figure('Name', 'Calibration')
+calib_ax1 = subplot(1, 2, 1);
+calib_ax2 = subplot(1 ,2 , 2);
+%Sample image
+figure('Name', 'Sample Image')
+sample_ax = axes('parent', gcf);
+
+%By default we asume the first image is the background image and the second
+%one is the calibration image. 
+%TODO. let the user can later adjust this using the GUI
+[bkgnd_img, img_map] = imread([path_to_folder img_files(1).name]);
+imshow(bkgnd_img, img_map, 'Parent', calib_ax1)
+calib_img = imread([path_to_folder img_files(2).name]);
+imshow(calib_img, img_map, 'Parent', calib_ax2)
+
+rnd_idx = 2 + randi(length(img_files)-2);
+sample_img = imread([path_to_folder img_files(rnd_idx).name]);
+imshow(sample_img, img_map, 'Parent', sample_ax)
+clear rnd_idx 
+
+%% Calibrate
+%transform into grayscale
+bkgnd_img = rgb2gray(bkgnd_img); 
+calib_img = rgb2gray(calib_img);
+
+%Resolution gets calibrated by the user, clicking in two points separated by 1 cm
+axes(calib_ax2) %pick the apropiate axes
+[x, y, ~] = impixel(calib_img); %Get two points separated by 1cm
+d = sqrt(diff(x)^2 + diff(y)^2);
+px2mm = 10/d; %resolution stored as number of pixels in 10mm
+
+%Store the results in a mat file and save the images used
+calib_path = './calibration/';
+if ~exist(calib_path, 'dir')
+    mkdir(calib_path)
+end
+imwrite(bkgnd_img, [calib_path 'background_img.tiff'], 'Compression', 'none');
+imwrite(calib_img, [calib_path 'calibration_img.tiff'], 'Compression', 'none');
+save([calib_path 'calib_info.mat'],'px2mm','bkgnd_img');
+clear d x y
+
+%% Process sample image
+%use a random image as a sandbox to run the algorithms before doing a batch
+%processing
+params.se = strel('disk', 5); %value of pixels used in the close algorithm
+params.nb = 8; %neighbourhood used
+params.px2mm = px2mm; %img resolution
+params.resample = 0.5;
+params.background_img = bkgnd_img;
+
+[D32, label_img, sample_img] = BV_quantification(sample_img,params);
+axes(sample_ax) %pick the apropiate axes
+imshow(label_img)
+
+%Store the params and results 
+label_path = './working_files/labelling/';
+if ~exist(label_path, 'dir')
+    mkdir(label_path)
+end
+imwrite(sample_img, [label_path 'sample_img.tiff'], 'Compression', 'none');
+imwrite(label_img, [label_path 'label_img.tiff'], 'Compression', 'none');
+save([label_path 'process_params.mat'],'params','bkgnd_img');
+
+%------------- END OF CODE --------------
--- quantification/BV_quantification.m
+++ quantification/BV_quantification.m
@@ -1,87 +1,53 @@
-%Set path to experiment folder
-clear all
-close all
-clc
-%%
-path = 'G:\My Drive\Imperial College\Experimental Data\Bubble Size Laser System\Second Try - Jan 2020\solids\';
-folders = dir(path); %get phases folder names
+function [D_32, newLabeledImage, img] = BV_quantification(img, params)
 
-%For each folder (acquisition) quantify d_32
-se = strel('disk',5); %value of pixels used in the close algorithm
-for i = 3:length(folders) %first two are '.' and '..'    
-    if folders(i).isdir
-        acq_folder = folders(i).name;
-        display(acq_folder)
-        %Calibration info (res and bkg image)
-        load([path acq_folder '/calib_info.mat']);
-        T = adaptthresh(Background,0.4,'ForegroundPolarity','dark');%use background correction as threshold
-        images = dir([path acq_folder '/*.JPG']);
-        
-        
-        %prompt = 'Number of first acquisition? : ';
-        %first_img_num = images(j).name;%input(prompt);
-        D = [];
-        E = [];
-        textprogressbar('Processing images ');
-        N = length(images);
-        for j=3:N
-            textprogressbar(j/N*100);
-            name_parts = regexpi(images(j).name, '_', 'split');
-            img_num = str2double(name_parts{2}(1:end-4));
-            I = imread([path acq_folder '/' images(j).name]);
-            I = rgb2gray(I);
-            I = I(1:2:end,1:2:end); %makes image half size
-            BW = imbinarize(I,T(1:2:end,1:2:end)); 
-            B = imclose(~BW,se);
-            B = imfill(B,'holes');
-            B = imclearborder(B);
-            R = -bwdist(~B);
-            %Ld = watershed(R);
-            %bw2 = B;
-            %bw2(Ld == 0) = 0;
-            mask = imextendedmin(R,8);
-            R2 = imimposemin(R,mask);
-            Ld2 = watershed(R2);
-            B(Ld2 == 0) = 0;
-            CH = bwconvhull(B,'objects');
-            R3=-bwdist(~CH);
-            mask = imextendedmin(R,8);
-            mask2 = imextendedmin(R3,8);
-            R4 = imimposemin(R3,mask2);
-            Ld3=watershed(R4);
-            CH(Ld3==0)=0;
-            CC = bwconncomp(CH,4); %TODO do some object separation, check "Watershed transform"
-            L = label2rgb(labelmatrix(CC));
-            S = regionprops(CC,'EquivDiameter','Area','Eccentricity','ConvexImage','Solidity'); %Eccentricity: 0 -> circle, 1 -> line
-            %TODO reject abnormal bubbles from quantification. check other "region props"
-            A = [S.Area]'; %column vector with areas
-            E_aux = [S.Eccentricity]'; %column vector with eccentricity
-            D_aux = [S.EquivDiameter]'; %column vector with diameters 
-            S_aux= [S.Solidity]';
-            idx = E_aux>0.85| S_aux<0.9; %abnormal bubbles: too stretch (E>0.85 , check this value!) 1 for water
-            allowableAreaIndexes = ~idx;
-            keeperIndexes = find(allowableAreaIndexes); 
-            keeperBlobsImage = ismember(bwlabel(CH), keeperIndexes);
-            newLabeledImage = label2rgb(bwlabel(keeperBlobsImage, 8));  
-            %fig1=figure('Color',[1 1 1]);
-            %imshowpair(I,newLabeledImage,'montage')
-            D_aux = D_aux(~idx)*px2mm*2; %record only normal bubbles
-            D = [D; D_aux]; %#ok
-            E_aux = E_aux(~idx);
-            E = [E; E_aux]; %#ok
-            
-        end
-        textprogressbar(' done');
-        %Acquisition's D_32
-        D_32 = sum(D.^3)/sum(D.^2); %in mm
-        u = round(D_32)+2;
-        %Acqusition's histograma EquivDiameter    
-        bubbles_psd(D,100,[0 u],'Eq. diameter [mm]'); %100 classes, show only between 0 and round(D_32)+2
-        title([acq_folder ' d_{32}=' num2str(D_32,'%1.2f')]);
-        export_fig([path acq_folder '_histogram.pdf'])
-        close 
+se = params.se; %strel object to perform binary operations
+nb = params.nb; %neighbourhood used
+px2mm = params.px2mm; %img resolution
+img_resample = params.resample;
+bknd_img = params.background_img;
 
-        save([path acq_folder '/quantification.mat'],'D','D_32','E');
-    end
+%use background correction as threshold
+T = adaptthresh(bknd_img, 0.4 , 'ForegroundPolarity', 'dark');
+
+if size(img,3)>1
+    img = rgb2gray(img);
+end
+img = imresize(img, img_resample); %resample img to make process faster
+BW = imbinarize(img,imresize(T, img_resample));
+B = imclose(~BW,se);
+B = imfill(B,'holes');
+B = imclearborder(B); %remove bubbles touching the border
+R = -bwdist(~B);
+
+mask = imextendedmin(R,nb);
+R2 = imimposemin(R,mask);
+Ld2 = watershed(R2);
+B(Ld2 == 0) = 0;
+CH = bwconvhull(B,'objects');
+R3 = -bwdist(~CH);
+mask = imextendedmin(R3,nb);
+R4 = imimposemin(R3,mask);
+Ld3 = watershed(R4);
+CH(Ld3==0) = 0;
+
+if nb>4
+    nb = 4;
 end
 
+CC = bwconncomp(CH,nb);
+S = regionprops(CC,'EquivDiameter','Area','Eccentricity','ConvexImage','Solidity'); %Eccentricity: 0 -> circle, 1 -> line
+
+%reject abnormal bubbles from quantification. check other "region props"
+A = [S.Area]'; %column vector with areas
+E = [S.Eccentricity]'; %column vector with eccentricity
+D = [S.EquivDiameter]'; %column vector with diameters
+S = [S.Solidity]';
+idx = E>0.85| S<0.9; %abnormal bubbles: too stretch (E>0.85 , check this value!) 1 for water
+allowableAreaIndexes = ~idx;
+D = D(~idx) * px2mm * 2; %WHY THE *2???
+keeperIndexes = find(allowableAreaIndexes);
+keeperBlobsImage = ismember(bwlabel(CH), keeperIndexes);
+newLabeledImage = label2rgb(bwlabel(keeperBlobsImage, nb));
+
+%Acquisition's D_32
+D_32 = sum(D.^3)/sum(D.^2); %in mm
