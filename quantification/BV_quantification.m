df15f120ac90c5d93c4356cf3a53d3287b5aea2b
frreyes1 <frreyes@uc.cl>
2021-03-01 06:59:54 +1000

    Added comments and optimised the default algorithm, plus some minor edits to the GUI
    Signed-off-by: frreyes1 <frreyes@uc.cl>

--- quantification/BV_quantification.m
+++ quantification/BV_quantification.m
@@ -8,7 +8,7 @@
 %    user or in the corresponding .config file
 %    
 % Outputs:
-%    D_32: D_32 of each bubble detected and segmented
+%    D: number array with the equivalent diameter, in mm, of each bubble detected and segmented
 %    L: labelled image resulting from the image processing algorithm
 %
 %
@@ -32,8 +32,9 @@
 %    along with Bubble Analyser.  If not, see <https://www.gnu.org/licenses/>.
 %
 %------------- BEGIN CODE --------------
-function [D_32, newLabeledImage] = BV_quantification(img, params)
+function [D, L_image] = BV_quantification(img, params)
 
+%Collect parameters from the structure
 se = strel('disk', params.Morphological_element_size); %strel object to perform binary operations
 nb = params.Neighbourhood_size; %neighbourhood used
 px2mm = params.px2mm; %img resolution
@@ -42,28 +43,34 @@ bknd_img = params.background_img;
 E_th = params.Eccentricity;
 S_th = params.Solidity;
 
-%use background correction as threshold
+%Resize images for making processing faster
+img = imresize(img, img_resample); %resample img to make process faster
+if size(img,3)>1
+    img = rgb2gray(img);
+end
+
+%use background correction to help the gray threshold
 if ~isempty(bknd_img)
     if size(bknd_img,3)>1
         bknd_img = rgb2gray(bknd_img);
     end    
     T = adaptthresh(bknd_img, 0.4 , 'ForegroundPolarity', 'dark');
 end
-
-if size(img,3)>1
-    img = rgb2gray(img);
-end
-img = imresize(img, img_resample); %resample img to make process faster
 if ~isempty(bknd_img)
     BW = imbinarize(img,imresize(T, img_resample));
 else
     BW = imbinarize(img);
 end
+
+%Use morphological operations to help make the bubbles solid
 B = imclose(~BW,se);
 B = imfill(B,'holes');
-B = imclearborder(B); %remove bubbles touching the border
-R = -bwdist(~B);
 
+%remove bubbles touching the borders
+B = imclearborder(B);
+
+%Now use watershed to separate bubbles that are overlapping
+R = -bwdist(~B);
 mask = imextendedmin(R,nb);
 R2 = imimposemin(R,mask);
 Ld2 = watershed(R2);
@@ -75,20 +82,23 @@ R4 = imimposemin(R3,mask);
 Ld3 = watershed(R4,nb);
 CH(Ld3==0) = 0;
 
+%Now list the detected objects and calculate geometric properties 
 CC = bwconncomp(CH,4);
-S = regionprops(CC,'EquivDiameter','Area','Eccentricity','ConvexImage','Solidity'); %Eccentricity: 0 -> circle, 1 -> line
+%Eccentricity: 0 -> circle, 1 -> line; Solidity = Area/ConvexArea
+S = regionprops(CC,'EquivDiameter','Eccentricity','Solidity');
 
-%reject abnormal bubbles from quantification. check other "region props"
-A = [S.Area]'; %column vector with areas
+%Reject abnormal objects, possibly unseparated bubbles
 E = [S.Eccentricity]'; %column vector with eccentricity
 D = [S.EquivDiameter]'; %column vector with diameters
-S = [S.Solidity]';
-idx = E>E_th| S<S_th; %abnormal bubbles: too stretch (E>0.85 , check this value!) 1 for water
+S = [S.Solidity]'; %
+idx = E>E_th | S<S_th; %abnormal bubbles: too stretched
+
+%Update label image
 allowableAreaIndexes = ~idx;
-D = D(~idx) * px2mm * 2; %WHY THE *2???
 keeperIndexes = find(allowableAreaIndexes);
 keeperBlobsImage = ismember(bwlabel(CH), keeperIndexes);
-newLabeledImage = label2rgb(bwlabel(keeperBlobsImage, nb));
+L_image = label2rgb(bwlabel(keeperBlobsImage, nb));
 
-%Acquisition's D_32
-D_32 = sum(D.^3)/sum(D.^2); %in mm
+%Return D, computed as sqrt(4*Area/pi), but in mm for later analysis
+%!!Remember we scaled down the image by some factor!!
+D = D(~idx) * px2mm * 1/img_resample; %now in mm
